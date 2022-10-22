239f381d20c91c6150e7f47988b64a2e4de38865
Diego Torres <die.torres.gonzalez@gmail.com>
2022-10-21 20:21:26 -0700

    Modificar campos en la base de datos, agregar modelo de evento

--- TestApp/models.py
+++ TestApp/models.py
@@ -10,7 +10,10 @@ class Presentacion(models.Model):
     departamento = models.CharField(max_length = 40)
     grado = models.TextChoices()
     modalidad = models.TextChoices()
-    edicion = models.DateField()
+    titulo = models.CharField()
+    autores = models.CharField()
+    resumen = models.FileField(upload_to = 'registros/resumenes/')
+    constancia = models.Filefield(upload_to = 'registros/constanciaas/')
     estatus = models.TextChoices()
 
 class Inicio(models.Model):
@@ -21,7 +24,7 @@ class Inicio(models.Model):
 
 class Programa(models.Model):
     titulo = models.CharField(max_length = 40)
-    pdf = models.CharField()
+    pdf = models.Filefield(upload_to = 'programa/')
 
 class Poster(models.Model):
     titulo = models.CharField(max_length = 40)
@@ -30,7 +33,8 @@ class Poster(models.Model):
 class Registro(models.Model):
     titulo = models.CharField(max_length = 40)
     mensaje = models.CharField()
-    link_resumen = models.URLField()
+    link_resumen = models.FileField(upload_to = 'registros/resumenes/')
+    descripcion = models.CharField()
 
 class Ubicacion(models.Model):
     titulo = models.CharField(max_length = 40)
@@ -47,19 +51,33 @@ class Edicion(models.Model):
     texto = models.CharField()
 
 class PlantillaBase(models.Model):
-    cartel = models.URLField()
+    cartel = models.FileField(upload_to = 'base/')
     titulo = models.CharField(max_length = 40)
     pie_texto = models.CharField()
     link_texto = models.CharField(max_length = 40)
     link_url = models.URLField()
 
-class Edicion(models.Model):
-    titulo = models.CharField()
+class IterAnteriores(models.Model):
+    titulo = models.CharField(max_length = 60)
     texto = models.CharField()
 
-class PlantillaBase(models.Model):
-    cartel = models.URLField()
-    titulo = models.CharField()
-    pie_texto = models.CharField()
-    link_texto = models.CharField()
-    link_url = models.URLField()
+    def get_events():
+        return Evento.objects.all()
+
+class Evento(models.Model):
+    nombre_evento = CharField()
+    inicio = models.ForeignKey('Inicio', on_delete = models.CASACADE,)
+    programa = models.ForeignKey('Programa', on_delete = models.CASACADE,)
+    poster = models.ForeignKey('Poster', on_delete = models.CASACADE,)
+    ubicacion = models.ForeignKey('Ubicacion', on_delete = models.CASACADE,)
+    contacto = models.ForeignKey('Contacto', on_delete = models.CASACADE,)
+    registro = models.ForeignKey('Registro', on_delete = models.CASACADE,)
+    anteriores = models.ForeignKey('IterAnteriores', on_delete = models.CASACADE,)
+    base = models.ForeignKey('PlantillaBase', on_delete = models.CASACADE,)
+    plantilla_constancias = models.FileField(upload_to = 'constancias/base/')
+    correo_comunicacion = models.EmailField()
+    correo_contrasena = models.CharField()
+
+
+
+
