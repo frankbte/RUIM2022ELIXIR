71c2bed8e70d07c543d789cc793fa0591357bc28
Diego Torres <die.torres.gonzalez@gmail.com>
2022-10-25 09:58:20 -0700

    Agregar view para borrar una edicion del evento

--- TestApp/views.py
+++ TestApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Evento
+from django.db import models
 
 
 
@@ -18,4 +19,28 @@ def ponencias(request):
 def ediciones(request):
     return render(request, 'TestApp/ediciones.html')
 
-# Create your views here.
+def remove_iteration(request):
+    event_name = request.POST.get("nombre_evento") #Desde el view, la seleccion de evento a borrar tiene que venir con ese nombre 
+                                                    #y que corresponda con el nombre en la db.
+    try:
+        event = Evento.objects.get(nombre_evento = event_name)
+
+        event.inicio.delete()
+        event.programa.delete()
+        event.poster.delete()
+        event.ubicaciones.delete()
+        event.contacto.delete()
+        event.registro.delete()
+        event.anteriores.delete()
+        event.base.delete()
+
+        event.delete()
+
+    except Entry.DoesNotExist:
+        println("Evento no existe")
+
+    return render(request, 'TestApp/home.html') #esto es solo mientras se construye la pagina en donde el administrador podra eliminar ediciones del evento
+
+
+
+
