8a5ca33de040b4da11045ca8d16aece806676841
Daniel8AG <danielrochoag@gmail.com>
2022-10-30 23:41:25 -0700

    Cambios en PresentaciÃonForm, de registro de ponencias

--- TestApp/forms.py
+++ TestApp/forms.py
@@ -9,7 +9,39 @@ class EventoForm(forms.ModelForm):
 class PresentacionForm(forms.ModelForm):
     class Meta:
         model = PresentacionRegistro
-        fields = "__all__"
+        fields = [
+            'presentacion_titulo', 
+            'resp',
+            'resp_email',
+            'a1',
+            'a2',
+            'a3',
+            'a4',
+            'a5',
+            'a6',
+            'a7',
+            'institucion',
+            'departamento',
+            'modalidad',
+            'resumen'
+            ]
+
+        labels = {
+            'presentacion_titulo': 'Titulo de la presentación',
+            'resp': 'Responsable de la presentación',
+            'resp_email': 'Correo electrónico del representante de la presentación',
+            'a1': 'Autor 1',
+            'a2': 'Autor 2',
+            'a3': 'Autor 3',
+            'a4': 'Autor 4',
+            'a5': 'Autor 5',
+            'a6': 'Autor 6',
+            'a7': 'Autor 7',
+            'institucion': 'Institución de procedencia',
+            'departamento': 'Departamento',
+            'modalidad': 'Tipo de presentación (ponencia o cartel)',
+            'resumen': 'Breve resumen de la presentación (archivo PDF)'
+        }
 
 class InicioPageForm(forms.ModelForm):
     class Meta:
