8d544947cfac36f3c3cf79396e3a280a2046a858
Diego Torres <die.torres.gonzalez@gmail.com>
2022-10-30 18:46:32 -0700

    added url from templates to settings.py

--- Test/settings.py
+++ Test/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Test.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["./TestApp/Templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
