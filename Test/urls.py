771075156e1e37646072270d6c67470404bf04d3
ErickNavarroM <name020609@gmail.com>
2022-11-18 18:31:13 -0700

    Permitir subida de archivos con forms (en directorio y BBDD)

--- Test/urls.py
+++ Test/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from TestApp import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'Test'
 
 urlpatterns = [
     path('', include('TestApp.urls', namespace='TestApp')),
     path('admindb/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
