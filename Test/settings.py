425a4d89fc5b425728afb795f4081700c6dff298
Diego Torres <die.torres.gonzalez@gmail.com>
2022-11-20 13:39:24 -0700

    arreglar crear iteracion

--- Test/settings.py
+++ Test/settings.py
@@ -121,13 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'archivos/'
 MEDIA_URL = 'TestApp/static/TestApp/archivos/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'TestApp/static/TestApp/archivos/')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'archivos'),
 )
 
 # Default primary key field type
